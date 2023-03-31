a02c2aa1802584c8e1368ff9bb09ba8849ffc789
Oshadha-basnayaka <oshadhabasnayaka69@gmail.com>
2023-04-01 00:46:16 +0530

    Update DashboadFormController.java

--- src/lk/ijse/d24/controller/DashboadFormController.java
+++ src/lk/ijse/d24/controller/DashboadFormController.java
@@ -1,8 +1,20 @@
 package lk.ijse.d24.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
 
 public class DashboadFormController {
+    public ImageView pane;
+
     public void onActionBtnReservation(ActionEvent actionEvent) {
+    }
+
+    public void onActionBtnStudent(ActionEvent actionEvent) {
+    }
+
+    public void onActionBtnRoom(ActionEvent actionEvent) {
+    }
+
+    public void onActionBtnUser(ActionEvent actionEvent) {
     }
 }
