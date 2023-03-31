4a8f8979d6e345d99ee6d6bb71c4ddad025140e8
Oshadha-basnayaka <oshadhabasnayaka69@gmail.com>
2023-04-01 00:35:18 +0530

    Create MainLoginFormController.java

---
+++ src/lk/ijse/d24/controller/MainLoginFormController.java
@@ -0,0 +1,14 @@
+package lk.ijse.d24.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class MainLoginFormController {
+    public JFXTextField txtUserName;
+    public JFXTextField txtPWord;
+    public AnchorPane pane;
+
+    public void onActionBtnLogin(ActionEvent actionEvent) {
+    }
+}
