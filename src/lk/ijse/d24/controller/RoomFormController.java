5f64694eccac8b3b8f673c0dd17e5f93325c72f9
Oshadha-basnayaka <oshadhabasnayaka69@gmail.com>
2023-04-05 17:40:07 +0530

    Update RoomFormController.java

--- src/lk/ijse/d24/controller/RoomFormController.java
+++ src/lk/ijse/d24/controller/RoomFormController.java
@@ -1,6 +1,8 @@
 package lk.ijse.d24.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import lk.ijse.d24.util.Navigation;
 import lk.ijse.d24.util.Routes;
@@ -9,6 +11,11 @@ import java.io.IOException;
 
 public class RoomFormController {
     public AnchorPane pane;
+    public TableView tblRoom;
+    public TextField txtRoomId;
+    public TextField txtRoomType;
+    public TextField txtRoomKeyMoney;
+    public TextField txtRoomQty;
 
     public void onActionBtnReservation(ActionEvent actionEvent) throws IOException {
         Navigation.navigate(Routes.RESERVATION, pane);
@@ -28,5 +35,17 @@ public class RoomFormController {
 
     public void onActionBtnSignOut(ActionEvent actionEvent) throws IOException {
         Navigation.navigate(Routes.LOGIN, pane);
+    }
+
+    public void onActionBtnRoomAdd(ActionEvent actionEvent) {
+    }
+
+    public void onActionBtnRoomSearch(ActionEvent actionEvent) {
+    }
+
+    public void onActionBtnRoomUpdate(ActionEvent actionEvent) {
+    }
+
+    public void onActionBtnRoomDelete(ActionEvent actionEvent) {
     }
 }
