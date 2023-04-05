1f4d8b1c2861d91eb3a1f9f94809d6e6a9238b87
Oshadha-basnayaka <oshadhabasnayaka69@gmail.com>
2023-04-05 17:40:18 +0530

    Update Student.java

--- src/lk/ijse/d24/entity/Student.java
+++ src/lk/ijse/d24/entity/Student.java
@@ -1,96 +1,33 @@
 package lk.ijse.d24.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.sql.Date;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Student {
     @Id
-    @Column(name = "s_id",length = 50)
+    @Column(name = "s_id", length = 50, nullable = false)
     private String id;
-    @Column(name = "s_name",length = 50)
+    @Column(name = "s_name", length = 50)
     private String name;
-    @Column(name = "s_contact",length = 50)
+    @Column(name = "s_contact", length = 50)
     private int contact;
-    @Column(name = "s_address",length = 50)
+    @Column(name = "s_address", length = 50)
     private String address;
-    @Column(name = "s_dob",length = 50)
+    @Column(name = "s_dob", length = 50)
     private Date dob;
-    @Column(name = "s_gender",length = 50)
+    @Column(name = "s_gender", length = 50)
     private String gender;
 
-    public Student() {
-    }
 
-    public Student(String id, String name, int contact, String address, Date dob, String gender) {
-        this.id = id;
-        this.name = name;
-        this.contact = contact;
-        this.address = address;
-        this.dob = dob;
-        this.gender = gender;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getContact() {
-        return contact;
-    }
-
-    public void setContact(int contact) {
-        this.contact = contact;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Date getDob() {
-        return dob;
-    }
-
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", contact=" + contact +
-                ", address='" + address + '\'' +
-                ", dob=" + dob +
-                ", gender='" + gender + '\'' +
-                '}';
-    }
 }
