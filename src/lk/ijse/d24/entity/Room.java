8f0c6f49f4fadc1acab8052f4e23471fd2e9f817
Oshadha-basnayaka <oshadhabasnayaka69@gmail.com>
2023-04-05 17:40:15 +0530

    Update Room.java

--- src/lk/ijse/d24/entity/Room.java
+++ src/lk/ijse/d24/entity/Room.java
@@ -1,4 +1,26 @@
 package lk.ijse.d24.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
 public class Room {
+    @Id
+    @Column(name = "r_id", length = 50, nullable = false)
+    private String id;
+    @Column(name = "r_type", length = 50)
+    private String roomType;
+    @Column(name = "r_keyMoney", length = 50)
+    private int keyMoney;
+    @Column(name = "r_qty", length = 50)
+    private int qty;
 }
