28f0e5f9fc28fda8667b33e00e4edb4820d5014c
Oshadha-basnayaka <oshadhabasnayaka69@gmail.com>
2023-04-07 23:54:18 +0530

    Create BoFactory.java

---
+++ src/lk/ijse/d24/bo/BoFactory.java
@@ -0,0 +1,36 @@
+package lk.ijse.d24.bo;
+
+import lk.ijse.d24.bo.custom.impl.ReservationBOimpl;
+import lk.ijse.d24.bo.custom.impl.RoomBOimpl;
+import lk.ijse.d24.bo.custom.impl.StudentBOimpl;
+import lk.ijse.d24.bo.custom.impl.UserBOimpl;
+
+public class BoFactory {
+    private static BoFactory boFactory;
+
+    private BoFactory() {
+    }
+
+    public static BoFactory getInstance() {
+        return
+                (boFactory == null) ? boFactory = new BoFactory() : boFactory;
+    }
+
+    public SuperBO getBO(BoType boType){
+        switch (boType){
+            case USER:
+                return new UserBOimpl();
+            case STUDENT:
+                return new StudentBOimpl();
+            case ROOM:
+                return new RoomBOimpl();
+            case RESERVATION:
+                return new ReservationBOimpl();
+
+            default:
+                System.out.println("Class not Found");
+                return null;
+
+        }
+    }
+}
