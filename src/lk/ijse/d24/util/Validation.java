a60dc62139b0ecd64f7c9f85d97d2be4b4dbb9b0
Oshadha-basnayaka <oshadhabasnayaka69@gmail.com>
2023-04-27 17:18:27 +0530

    Create Validation.java
    u

---
+++ src/lk/ijse/d24/util/Validation.java
@@ -0,0 +1,31 @@
+package lk.ijse.d24.util;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.paint.Paint;
+
+import javax.swing.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+
+    public static boolean validateLettersOnly(String text){
+        Pattern pattern = Pattern.compile("^[A-Za-z]+(?:\\s[A-Za-z]+)*$");
+        Matcher matcher = pattern.matcher(text);
+
+        return matcher.matches();
+    }
+    public static boolean validateNumbersOnly(String text){
+        Pattern pattern = Pattern.compile("^[0-9]+$\n");
+        Matcher matcher = pattern.matcher(text);
+
+        return matcher.matches();
+    }
+
+    public static boolean validateDateOnly(String text){
+        Pattern pattern = Pattern.compile("^[0-9]+$\n");
+        Matcher matcher = pattern.matcher(text);
+
+        return matcher.matches();
+    }
+}
